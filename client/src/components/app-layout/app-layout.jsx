d9c647ae34f98ea4ee5ed4c8b6ab7d2cb91bc788
keval <kevalradadiya9825@gmail.com>
2023-11-27 11:38:55 +0530

    unnecessary code removal

--- client/src/components/app-layout/app-layout.jsx
+++ client/src/components/app-layout/app-layout.jsx
@@ -1,7 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { isLoggedIn } from "../../utils/helper";
 import Path from "../../constants/local-path";
-import Sidebar from "./sidebar";
 import Header from "./header";
 
 function ProtectedRoute() {
@@ -21,7 +20,6 @@ function HomeRoute() {
 function AppLayout({ children }) {
   return (
     <div className="flex h-screen">
-      <Sidebar />
       <div className="flex-1 h-full overflow-x-hidden">
         <Header />
         <main className="relative top-20 h-[calc(100%_-_5rem)] overflow-y-auto bg-gray-mercury px-5 py-4 scrollbar-gutter">
--- client/src/components/app-layout/header.jsx
+++ client/src/components/app-layout/header.jsx
@@ -3,14 +3,8 @@ import { useNavigate } from "react-router-dom";
 import Path from "../../constants/local-path";
 
 import { removeSession, getSession } from "../../utils/helper";
-import DownloadCSV from "../download-CSV";
 
 const Header = () => {
-  const user = getSession().user;
-  console.log(user, "--------");
-  const name = "keval";
-  // const { name } = user;
-
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -25,11 +19,10 @@ const Header = () => {
       <header className="items-center fixed h-20 bg-blue-300 px-5 py-4 flex w-[calc(100%_-_8rem)] z-20">
         <div className="w-2/5 flex items-center">
           <p className="text-2xl font-semibold text-black-light">
-            Admin Panel - RK Ninjas - Placement cell
+            ReachHUB Interview Test
           </p>
         </div>
         <div className="flex w-3/5 justify-end mr-16 items-center">
-          <DownloadCSV />
           <button
             type="button"
             className="ml-10 text-white bg-[#050708] hover:bg-[#050708]/80 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-6 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 dark:focus:ring-gray-600 mr-2"
